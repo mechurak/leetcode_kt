1248577d7b6b4ebee30ff052c55279d29b901021
sshim <mechurak@gmail.com>
2020-09-02 21:28:40 +0900

    Solve q4, q5

--- src/q0004_MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.kt
+++ src/q0004_MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.kt
@@ -3,6 +3,43 @@ package q0004_MedianOfTwoSortedArrays
 
 class Solution {
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-        return 0.0
+        val l = nums1.size + nums2.size
+        return if (l % 2 == 0) {
+            val v1 = kth(nums1, nums2, l / 2 - 1)
+            val v2 = kth(nums1, nums2, l / 2)
+            (v1 + v2) / 2.0
+        } else {
+            kth(nums1, nums2, l / 2).toDouble()
+        }
+    }
+
+    fun kth(a: IntArray, b: IntArray, k: Int): Int {
+        if (a.isEmpty()) {
+            return b[k]
+        }
+        if (b.isEmpty()) {
+            return a[k]
+        }
+
+        var left = a
+        var right = b
+        var ia = a.size / 2
+        var ib = b.size / 2
+        val ma = a[ia]
+        val mb = b[ib]
+
+        if (mb < ma) {
+            left = b
+            right = a
+            val temp = ia
+            ia = ib
+            ib = temp
+        }
+
+        return if (ia + ib < k) {
+            kth(left.sliceArray(ia + 1 until left.size), right, k - ia - 1)
+        } else {
+            kth(left, right.sliceArray(0 until ib), k)
+        }
     }
 }
--- src/q0005_LongestPalindromicSubstring/LongestPalindromicSubstring.kt
+++ src/q0005_LongestPalindromicSubstring/LongestPalindromicSubstring.kt
@@ -2,6 +2,27 @@ package q0005_LongestPalindromicSubstring
 
 class Solution {
     fun longestPalindrome(s: String): String {
-        return ""
+        var answer = ""
+        s.forEachIndexed{ i, c ->
+            val odd_answer = getCurAnswer(s, i, i)
+            if (odd_answer.length > answer.length) {
+                answer = odd_answer
+            }
+            val even_answer = getCurAnswer(s, i, i+1)
+            if (even_answer.length > answer.length) {
+                answer = even_answer
+            }
+        }
+        return answer
+    }
+
+    fun getCurAnswer(s: String, l: Int, r: Int): String {
+        var leftIndex = l
+        var rightIndex = r
+        while (leftIndex >= 0 && rightIndex < s.length && s[leftIndex] == s[rightIndex]) {
+            leftIndex -= 1
+            rightIndex += 1
+        }
+        return s.substring(leftIndex+1 until rightIndex)
     }
 }
